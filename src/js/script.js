8f2b865912f24b0c61dd5c70da3bbbe5d13a3375
Mohit Sharma <iamxguy352@gmail.com>
2022-07-13 01:07:52 +0530

    Inital Commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// const swiper = new Swiper(...);
+let swiper = new Swiper('.projects', {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
