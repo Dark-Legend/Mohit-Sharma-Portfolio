ffc8bff9e53176119dbcb44122d8eff59eb65d24
Mohit Sharma <iamxguy352@gmail.com>
2022-07-14 20:59:07 +0530

    Completed half media query for small devices

--- src/js/main.js
+++ src/js/main.js
@@ -1,91 +1,91 @@
-'use strict'
+// 'use strict'
 
-// ==========scroll on reveal=====================
+// // ==========scroll on reveal=====================
 
-const sr = ScrollReveal({
-  origin: 'top',
-  distance: '90px',
-  duration: 2000,
-  reset: true,
-})
+// const sr = ScrollReveal({
+//   origin: 'top',
+//   distance: '90px',
+//   duration: 2000,
+//   reset: true,
+// })
 
-// ==================scroll home===================
-sr.reveal('.logo');
-sr.reveal('.list');
-sr.reveal('.heading-text', {});
-sr.reveal('.arrow', {delay: 200});
-sr.reveal('.header-img', {origin: 'right',delay: 400});
+// // ==================scroll home===================
+// sr.reveal('.logo');
+// sr.reveal('.list');
+// sr.reveal('.heading-text', {});
+// sr.reveal('.arrow', {delay: 200});
+// sr.reveal('.header-img', {origin: 'right',delay: 400});
 
 
-// ============scroll about=======================
-sr.reveal('.about-heading', {delay: 500});
-sr.reveal('.circle', {delay: 500});
-sr.reveal('.intro-text', {delay: 300});
-sr.reveal('.desigination', {delay: 400});
-sr.reveal('.paragraph', {delay: 500});
-sr.reveal('.links-icon', {
-  delay: 600,
-  interval: 200
-});
-sr.reveal('.download-btn',{
-  delay: 650,
-  interval: 250
-});
+// // ============scroll about=======================
+// sr.reveal('.about-heading', {delay: 500});
+// sr.reveal('.circle', {delay: 500});
+// sr.reveal('.intro-text', {delay: 300});
+// sr.reveal('.desigination', {delay: 400});
+// sr.reveal('.paragraph', {delay: 500});
+// sr.reveal('.links-icon', {
+//   delay: 600,
+//   interval: 200
+// });
+// sr.reveal('.download-btn',{
+//   delay: 650,
+//   interval: 250
+// });
 
-// ==============scroll skills=====================
+// // ==============scroll skills=====================
 
-sr.reveal('.skill');
-sr.reveal('.skill-icons', {
-  distance:'20px',
-  delay: 50,
-  interval: 100
-});
+// sr.reveal('.skill', {delay:50});
+// sr.reveal('.skill-icons', {
+//   distance:'20px',
+//   delay: 100,
+//   interval: 100
+// });
 
-// =============scroll projects==================
+// // =============scroll projects==================
 
-sr.reveal('.project-text');
-sr.reveal('.bb', {interval: 200});
-sr.reveal('.pro');
+// sr.reveal('.project-text');
+// sr.reveal('.bb', {interval: 200});
+// sr.reveal('.pro');
 
-// =============scroll blog============
-sr.reveal('.blog-text');
-sr.reveal('.blog1',{
-  origin: 'left',
-  delay: 200
-});
+// // =============scroll blog============
+// sr.reveal('.blog-text');
+// sr.reveal('.blog1',{
+//   origin: 'left',
+//   delay: 200
+// });
 
-sr.reveal('.blog2',{
-  origin: 'right',
-  delay: 200
-});
+// sr.reveal('.blog2',{
+//   origin: 'right',
+//   delay: 200
+// });
 
-// ===========scroll contact=================
+// // ===========scroll contact=================
 
-sr.reveal('.contact-text');
-sr.reveal('.details',{
-  origin: 'left',
-  delay: 200
-});
+// sr.reveal('.contact-text');
+// sr.reveal('.details',{
+//   origin: 'left',
+//   delay: 200
+// });
 
-sr.reveal('.contact-form',{
-  origin: 'right',
-  delay:200
-});
+// sr.reveal('.contact-form',{
+//   origin: 'right',
+//   delay:200
+// });
 
-sr.reveal('.name', {delay:300,origin: 'right'});
-sr.reveal('.email', {delay: 400, origin: 'right'});
-sr.reveal('.message',{
-  delay: 500,
-  origin: 'right'
-});
-sr.reveal('.button',{
-  delay: 450,
-  origin: 'right'
-});
+// sr.reveal('.name', {delay:300,origin: 'right'});
+// sr.reveal('.email', {delay: 400, origin: 'right'});
+// sr.reveal('.message',{
+//   delay: 500,
+//   origin: 'right'
+// });
+// sr.reveal('.button',{
+//   delay: 450,
+//   origin: 'right'
+// });
 
-// ================scroll footer====================
+// // ================scroll footer====================
 
-sr.reveal('.about-me');
-sr.reveal('.explore');
-sr.reveal('.social');
+// sr.reveal('.about-me');
+// sr.reveal('.explore');
+// sr.reveal('.social');
 
