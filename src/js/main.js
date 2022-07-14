352a4c298d73a49c1ce9aaea2419cfba41f64cc3
Mohit Sharma <iamxguy352@gmail.com>
2022-07-14 17:22:50 +0530

    Implementing scroll on reveal

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// ==========scroll on reveal=====================
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '90px',
+  duration: 2000,
+  reset: true
+})
+
+// ==================scroll home===================
+
+sr.ScrollReveal('.home', {})
